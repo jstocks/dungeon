c73117e85b81c56cf01caa98a58831e11efdf158
jstocks <GitHubUW33>
2020-12-08 13:23:54 -0800

    updating room in jeff-branch

--- room.py
+++ room.py
@@ -1,134 +1,76 @@
-from door import Door
-
-
 class Room:
-    def __init__(self, *args, **kwargs):
-        for arg in args:
-            if arg == "H":
-                self.__has_healing_portion = True
-            if arg == "X":
-                self.__has_pit = True
-            if arg == "A":
-                self.__has_pillar_A = True
-            if arg == "E":
-                self.__has_pillar_E = True
-            if arg == "I":
-                self.__has_pillar_I = True
-            if arg == "P":
-                self.__has_pillar_P = True
-            if arg == "V":
-                self.__has_vision = True
-            if arg == "O":
-                self.__exit = True
-            if arg == "i":
-                self.__entrance = True
-            if arg == "":
-                self.__space = True
-            if arg == "M":
-                self.__has_healing_portion = True
-                self.__has_pillar = True
-                self.__has_vision = True
+    def __init__(self):
+        self.__healing_portion = False
+        self.__pit = False
+        self.__pillar = "No pillar"
+        self.__vision = False
+        self.__exit = False
+        self.__entrance = False
+        self.__impassable = False
+        self.__visited = False
+        self.__item_count = 0
 
-        for key, value in kwargs.items():
-            if key == "up":
-                self.__up = Door(value)
-            if key == "down":
-                self.__down = Door(value)
-            if key == "left":
-                self.__left = Door(value)
-            if key == "right":
-                self.__right = Door(value)
+    def get_healing_portion(self):
+        return self.__healing_portion
 
-    def has_healing_portion(self):
-        try:
-            return self.__has_healing_portion is True
-        except AttributeError:
-            print("Room doesn't have Healing Portion")
+    def set_healing_portion(self):
+        self.__healing_portion = True
+        self.__item_count += 1
 
-    def has_pit(self):
-        try:
-            return self.__has_pit is True
-        except AttributeError:
-            print("Room doesn't have pit")
+    def get_pit(self):
+        return self.__pit
 
-    def has_pillar_a(self):
-        try:
-            return self.__has_pillar_A is True
-        except AttributeError:
-            print("Room doesn't have pillar A")
+    def set_pit(self):
+        self.__pit = True
 
-    def has_pillar_e(self):
-        try:
-            return self.__has_pillar_E is True
-        except AttributeError:
-            print("Room doesn't have pillar E")
+    def get_pillar(self):
+        return self.__pillar
 
-    def has_pillar_i(self):
-        try:
-            return self.__has_pillar_I is True
-        except AttributeError:
-            print("Room doesn't have pillar I")
+    def set_pillar(self):
+        self.__pillar = True
+        self.__item_count += 1
 
-    def has_pillar_p(self):
-        try:
-            return self.__has_pillar_P is True
-        except AttributeError:
-            print("Room doesn't have pillar P")
+    def get_vision(self):
+        return self.__vision
 
-    def has_vision(self):
-        try:
-            return self.__has_vision is True
-        except AttributeError:
-            print("Room doesn't have vision")
+    def set_vision(self):
+        self.__vision = True
+        self.__item_count += 1
 
-    def is_room_empty(self):
-        try:
-            return self.__space is True
-        except AttributeError:
-            print("Room is NOT an empty room")
+    def set_exit(self):
+        self.__exit = True
 
-    def has_entrance(self):
-        try:
-            return self.__entrance is True
-        except AttributeError:
-            print("Room has no entrance")
+    def is_exit(self):
+        return self.__exit
 
-    def has_exit(self):
-        try:
-            return self.__exit is True
-        except AttributeError:
-            print("Room has no exit")
+    def set_entrance(self):
+        self.__entrance = True
 
-    def move_up(self):
-        try:
-            return self.__up.is_lock() is True
-        except AttributeError:
-            print("You can't move up")
+    def set_impassable(self):
+        self.__impassable = True
 
-    def move_down(self):
-        try:
-            return self.__down.is_lock() is True
-        except AttributeError:
-            print("You can move down")
+    def can_enter(self):
+        return not self.__impassable
 
-    def move_left(self):
-        try:
-            return self.__left.is_lock() is True
-        except AttributeError:
-            print("You can't move to the left")
+    def set_visited(self):
+        self.__visited = True
 
-    def move_right(self):
-        try:
-            return self.__right.is_lock() is True
-        except AttributeError:
-            print("You can't move to the right")
+    def has_been_visited(self):
+        return self.__visited is True
 
+    def is_multiple_item(self):
+        return self.__item_count > 1
 
-inside = {"H", "V", "i"}
-door = {'up': True, 'down': False, 'left': False, 'right': False}
-rm = Room(inside, door)
+    def __str__(self):
+        return "Healing Portion: " + str(self.__healing_portion) + "\n" \
+            + "Pit: " + str(self.__pit) + "\n" \
+            + "Pillar: " + str(self.__pillar) + "\n" \
+            + "Vision: " + str(self.__pillar) + "\n" \
+            + "Exit: " + str(self.__exit) + "\n" \
+            + "Entrance: " + str(self.__entrance) + "\n" \
+            + "Impassable: " + str(self.__impassable) + "\n" \
+            + "Visited: " + str(self.__visited)
 
-rm.has_pit()
 
 
 
