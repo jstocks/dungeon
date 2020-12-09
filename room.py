927dbe014654edae98e7c4a453231208dc0233ac
dturco <73612631+dturco@users.noreply.github.com>
2020-12-08 20:54:09 -0800

    Add files via upload
    New room.py

--- room.py
+++ room.py
@@ -1,9 +1,15 @@
+import random
+
+
 class Room:
     def __init__(self):
         self.__healing_potion = False
         self.__pit = False
-        self.__pillar = "No pillar"
-        self.__vision = False
+        self.__pillar_a = False
+        self.__pillar_e = False
+        self.__pillar_i = False
+        self.__pillar_p = False
+        self.__vision_potion = False
         self.__exit = False
         self.__entrance = False
         self.__impassable = False
@@ -22,19 +28,41 @@ class Room:
 
     def set_pit(self):
         self.__pit = True
-
-    def get_pillar(self):
-        return self.__pillar
-
-    def set_pillar(self):
-        self.__pillar = True
         self.__item_count += 1
 
-    def get_vision(self):
-        return self.__vision
+    def get_pillar_a(self):
+        return self.__pillar_a
 
-    def set_vision(self):
-        self.__vision = True
+    def set_pillar_a(self):
+        self.__pillar_a = True
+        self.__item_count += 1
+
+    def get_pillar_e(self):
+        return self.__pillar_e
+
+    def set_pillar_e(self):
+        self.__pillar_e = True
+        self.__item_count += 1
+
+    def get_pillar_i(self):
+        return self.__pillar_i
+
+    def set_pillar_i(self):
+        self.__pillar_i = True
+        self.__item_count += 1
+
+    def get_pillar_p(self):
+        return self.__pillar_p
+
+    def set_pillar_p(self):
+        self.__pillar_p = True
+        self.__item_count += 1
+
+    def get_vision_potion(self):
+        return self.__vision_potion
+
+    def set_vision_potion(self):
+        self.__vision_potion = True
         self.__item_count += 1
 
     def set_exit(self):
@@ -55,6 +83,9 @@ class Room:
     def set_visited(self):
         self.__visited = True
 
+    def reset_visited(self):
+        self.__visited = False
+
     def has_been_visited(self):
         return self.__visited is True
 
@@ -62,11 +93,17 @@ class Room:
         return self.__item_count > 1
 
     def __str__(self):
-        return "Healing Potion: " + str(self.__healing_potion) + "\n" \
+        return "Healing Portion: " + str(self.__healing_potion) + "\n" \
             + "Pit: " + str(self.__pit) + "\n" \
             + "Pillar: " + str(self.__pillar) + "\n" \
-            + "Vision: " + str(self.__pillar) + "\n" \
+            + "Vision: " + str(self.__vision_potion) + "\n" \
             + "Exit: " + str(self.__exit) + "\n" \
             + "Entrance: " + str(self.__entrance) + "\n" \
             + "Impassable: " + str(self.__impassable) + "\n" \
             + "Visited: " + str(self.__visited)
+
+
+
+
+
+
